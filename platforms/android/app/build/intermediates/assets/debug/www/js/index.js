5212374d91dd0b7fe1e1122952e135334b84f161
thomas <nos18de@gmail.com>
2019-10-19 20:14:38 -0400

    first calculator v1

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+$(document).ready(function(){
+    //take html tags with assigned values and set to variable.
+    //set textarea to the value of "blank", and add to values.
+    
+    /* $('#win').keydown(function(event) { 
+        return false;
+    }); */
+
+    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#add, #subtract, #multiply, #divide, #power, #dot, #para1, #para2').click(function(){
+        var v = $(this).val();
+        var total = $('#win').val($('#win').val() + v); 
+    });
+    
+    //clicking equal sign evaluates the textarea
+    $('#equal').click(function(){
+        $('#win').val(eval($('#win').val()));
+    });
+  
+    
+    $('#clear').click(function(){
+        $('#win').val('');
+    });
+    
+      
+    $('#backspace').click(function(){
+        $('#win').val($('#win').val().substring(0, $('#win').val().length - 1));
+    });
+});
